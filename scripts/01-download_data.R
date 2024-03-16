81042f85b898c7985555501854533e34f0d5ebf9
aryamansuri <77459064+aryamansuri@users.noreply.github.com>
2024-03-16 13:03:49 -0400

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,28 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Project Gutenberg using gutenberg_download() from gutenbergr.
+# Author:  Aryaman Suri
+# Date: 12 March 2024 
+# Contact: aryaman.suri@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: The gutenbergr package
+# Any other information needed? no.
 
 
 #### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(gutenbergr)# Load the 'gutenbergr' package
+library(tidyverse)# Load the 'tidyverse' package.
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
 
+# Downloads the text of Villette by Charlotte Bronte from the Project Gutenberg.
+gutenberg_id_of_villette <- 9182
 
+villette <-
+  gutenberg_download(
+    gutenberg_id = gutenberg_id_of_villette,
+    mirror = "https://gutenberg.pglaf.org/"
+  )
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(villette, "data/raw_data/raw_data.csv") # Saves the raw dataset as a CSV file in the specified directory.
